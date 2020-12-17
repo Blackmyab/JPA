c2a96058ab8c3821524f646c3de7435317f5ea66
cesar <brycezs.92@gmail.com>
2020-12-16 19:36:55 -0500

    muebleria-jpa
    beta

---
+++ src/scripts/carouselData.js
@@ -0,0 +1,41 @@
+const carouselData = [
+    {
+        title: `Formas innovadoras de Decorar`,
+        paragraph: `Brindamos calidad, comodidad y estilo inigualables. \
+        Nuestros expertos combinan\
+        forma y función para dar vida a su visión.\
+        Crea una Locacion a tu estilo con nuestra colección ,\
+        y haz de tu propiedad un reflejo de ti y de lo que amas.`,
+        images: {
+            desktop: `./src/assets/images/desktop-image-hero-1.jpg`,
+            mobile: `./src/assets/images/mobile-image-hero-1.jpg`
+        }
+    },
+    {
+        title: `Estamos disponibles para todo el mundo`,
+        paragraph: `Con nuestras tiendas, es fácil para ti \
+        encontrar muebles para su hogar o lugar de trabajo,\
+        Estamos en la mayoría de las ciudades importantes de todo el país.\
+        .¿Alguna pregunta?\
+        No dude en contactarnos hoy.`,
+        images: {
+            desktop: `./src/assets/images/desktop-image-hero-2.jpg`,
+            mobile: `./src/assets/images/mobile-image-hero-2.jpg`
+        }
+    },
+    {
+        title: `Fabricado con los mejores materiales`,
+        paragraph: `Nuestra moderna tienda de muebles proporciona \
+        un alto nivel de calidad. Nuestra empresa ha invertido en tecnología avanzada\
+        para garantizar que cada producto se haga tan perfecto y\
+        o más consistente posible\
+        Entendemos lo que los clientes quieren para su \
+        hogar y oficina.`,
+        images: {
+            desktop: `./src/assets/images/desktop-image-hero-3.jpg`,
+            mobile: `./src/assets/images/mobile-image-hero-3.jpg`
+        }
+    }
+];
+
+export default carouselData;
